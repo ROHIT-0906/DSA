84ca65b148d626d0266169aa31a5bd60f05d5e20
ROHIT_0906 <rohitmakam09@gmail.com>
2026-06-29 22:16:35 +0530

    bellmanford&primsAlgo

--- Graph/GraphB.java
+++ Graph/GraphB.java
@@ -311,31 +311,107 @@ public class GraphB {
 
     }
 
+    public static void bellmanFord(ArrayList<Edge>[] graph, int src){
+        int dist[] = new int[graph.length];
+        for(int i=0; i<dist.length; i++){
+            if(i != src){
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        int V = graph.length;
+
+        for(int i=0; i<V-1; i++){
+            for(int j=0; j<graph.length; j++){
+                for(int k=0; k<graph[j].size(); k++){
+                    Edge e = graph[j].get(k);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    if(dist[u]+wt < dist[v]){
+                        dist[v] = dist[u]+wt;
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<dist.length; i++){
+            System.out.print(dist[i]+" ");
+        }
+
+    }
+
+    public static class Pair2 implements Comparable<Pair2>{
+        int vertex;
+        int cost;
+
+        public Pair2(int vertex, int cost){
+            this.vertex = vertex;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Pair2 p2){
+            return this.cost - p2.cost;
+        }
+
+    }
+
+    public static void  primsAlgo(ArrayList<Edge>[] graph){
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair2> pq = new PriorityQueue<>();
+        pq.add(new Pair2(0, 0));
+        int finalCost = 0;
+
+        while(!pq.isEmpty()){
+            Pair2 curr = pq.remove();
+            if(!vis[curr.vertex]){
+                vis[curr.vertex] = true;
+                finalCost += curr.cost;
+                for(int i=0;i<graph[curr.vertex].size(); i++){
+                    Edge e = graph[curr.vertex].get(i);
+                    pq.add(new Pair2(e.dest, e.wt));
+                }
+            }
+        }
+        System.out.println("Final cost of MST: "+finalCost);
+    }
+
+
     public static void main(String[] args) {
-        int V = 5;
+        int V = 4;
         ArrayList<Edge>[] graph = new ArrayList[V];
 
         for(int i=0; i<V; i++){
             graph[i] = new ArrayList<>();
         }
 
-        graph[0].add(new Edge(0, 1,2));
+        graph[0].add(new Edge(0, 1,10));
+        graph[0].add(new Edge(0, 2,15));
+        graph[0].add(new Edge(0, 3,30));
         // graph[0].add(new Edge(0, 3));
-        graph[0].add(new Edge(0, 2,4));
+        // graph[0].add(new Edge(0, 2,4));
         // graph[0].add(new Edge(0, 3));
 
-        graph[1].add(new Edge(1, 2,-4));
+        graph[1].add(new Edge(1, 0,10));
+        graph[1].add(new Edge(1, 3,40));
+        // graph[1].add(new Edge(1, 2,-4));
         // graph[1].add(new Edge(1, 3,7));
     
 
-        graph[2].add(new Edge(2,3, 2));
+        graph[2].add(new Edge(2,0, 15));
+        graph[2].add(new Edge(2,3, 50));
+        // graph[2].add(new Edge(2,3, 2));
         // graph[2].add(new Edge(2, 4));
     
 
-        // graph[3].add(new Edge(3, 0));
-        graph[3].add(new Edge(3, 4,4));
+        graph[3].add(new Edge(3, 0,30));
+        graph[3].add(new Edge(3, 1,40));
+        graph[3].add(new Edge(3, 2,50));
+        // graph[3].add(new Edge(3, 4,4));
 
-        graph[4].add(new Edge(4, 1,-1));
+        // graph[4].add(new Edge(4, 1,-1));
         // graph[4].add(new Edge(4, 5,5));
 
 
@@ -361,7 +437,11 @@ public class GraphB {
 
         // findAllPath(graph, 5, 1, "");
 
-        dijkstra(graph, 0);
+        // dijkstra(graph, 0);
+
+        // bellmanFord(graph, 0);
+
+        primsAlgo(graph);
 
     }
 
