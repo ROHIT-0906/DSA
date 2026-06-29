f1b27bb53d0b42ad75a7cda18560629cc0f4be16
ROHIT_0906 <rohitmakam09@gmail.com>
2026-06-29 13:42:18 +0530

    Graph2

--- Graph/GraphB.java
+++ Graph/GraphB.java
@@ -179,33 +179,81 @@ public class GraphB {
 
     }
 
-    public static void topSort(ArrayList<Edge>[] graph){
-        boolean vis[] = new boolean[graph.length];
-        Stack<Integer> s = new Stack<>();
+    // public static void topSort(ArrayList<Edge>[] graph){
+    //     boolean vis[] = new boolean[graph.length];
+    //     Stack<Integer> s = new Stack<>();
 
+    //     for(int i=0; i<graph.length; i++){
+    //         if(!vis[i]){
+    //             topSortUtil(graph, i ,vis, s);
+    //         }
+    //     }
+
+    //     while(!s.isEmpty()){
+    //         System.out.print(s.pop()+" ");
+    //     }
+    // }
+
+    // public static void topSortUtil(ArrayList<Edge>[] graph, int curr, boolean vis[], Stack<Integer> s){
+    //     vis[curr] = true;
+
+    //     for(int i=0; i<graph[curr].size(); i++){
+    //         Edge e = graph[curr].get(i);
+    //         if(!vis[e.dest]){
+    //             topSortUtil(graph, e.dest, vis, s);
+    //         }
+    //     }
+    //     s.push(curr);
+    // }
+
+    public static void calDeg(ArrayList<Edge>[] graph, int indeg[]){
         for(int i=0; i<graph.length; i++){
-            if(!vis[i]){
-                topSortUtil(graph, i ,vis, s);
+            for(int j=0; j<graph[i].size(); j++){
+                Edge e = graph[i].get(j);
+                indeg[e.dest]++;
             }
-        }
-
-        while(!s.isEmpty()){
-            System.out.print(s.pop()+" ");
         }
     }
 
-    public static void topSortUtil(ArrayList<Edge>[] graph, int curr, boolean vis[], Stack<Integer> s){
-        vis[curr] = true;
+    public static void topSort(ArrayList<Edge>[] graph){
+        int indeg[] = new int[graph.length];
+        calDeg(graph, indeg);
+        Queue<Integer> q = new LinkedList<>();
 
-        for(int i=0; i<graph[curr].size(); i++){
-            Edge e = graph[curr].get(i);
-            if(!vis[e.dest]){
-                topSortUtil(graph, e.dest, vis, s);
+        for(int i=0; i<indeg.length; i++){
+            if(indeg[i] == 0){
+                q.add(i);
             }
         }
-        s.push(curr);
+
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            System.out.print(curr+" ");
+
+            for(int i=0; i<graph[curr].size(); i++){
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+                if(indeg[e.dest] == 0){
+                    q.add(e.dest);
+                }
+            }
+
+        }
+
     }
 
+    public static void findAllPath(ArrayList<Edge>[] graph, int src, int dest, String path){
+        if(src == dest){
+            System.out.println(path+dest);
+            return;
+        }
+
+        for(int i=0; i<graph[src].size(); i++){
+            Edge e = graph[src].get(i);
+            findAllPath(graph, e.dest, dest, path+src+" ");
+        }
+
+    }
 
     public static void main(String[] args) {
         int V = 6;
@@ -216,7 +264,7 @@ public class GraphB {
         }
 
         // graph[0].add(new Edge(0, 1));
-        // graph[0].add(new Edge(0, 3));
+        graph[0].add(new Edge(0, 3));
         // graph[0].add(new Edge(0, 2));
         // graph[0].add(new Edge(0, 3));
 
@@ -228,8 +276,8 @@ public class GraphB {
         // graph[2].add(new Edge(2, 4));
     
 
+        // graph[3].add(new Edge(3, 0));
         graph[3].add(new Edge(3, 1));
-        // graph[3].add(new Edge(3, 4));
 
         graph[4].add(new Edge(4, 0));
         graph[4].add(new Edge(4, 1));
@@ -253,7 +301,9 @@ public class GraphB {
 
         // System.out.println(isCycle(graph));
 
-        topSort(graph);
+        // topSort(graph);
+
+        findAllPath(graph, 5, 1, "");
 
     }
 
